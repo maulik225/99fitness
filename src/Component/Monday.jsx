26738882cd6523e20ac7c9c8a37599c0c8c66aa1
maulik225 <msompura00@gmail.com>
2022-05-21 21:57:58 +0530

    improved code structure

--- src/Component/Monday.jsx
+++ src/Component/Monday.jsx
@@ -4,40 +4,19 @@ import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
 import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
-import Collapse from '@mui/material/Collapse';
-import Avatar from '@mui/material/Avatar';
-import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import { red } from '@mui/material/colors';
 import monday from '../../src/JsonSource/monday.json';
 
 import ReactPlayer from 'react-player'
 
-const ExpandMore = styled((props) => {
-  const { expand, ...other } = props;
-  return <IconButton {...other} />;
-})(({ theme, expand }) => ({
-  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
-  marginLeft: 'auto',
-  transition: theme.transitions.create('transform', {
-    duration: theme.transitions.duration.shortest,
-  }),
-}));
-
 export default function Monday() {
-  const [expanded, setExpanded] = React.useState(false);
   const [total] = React.useState(monday);
-
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
-  };
 
   return (
     <>
       {total.map((item, index) => (
         <div style={{ marginBottom: "24px" }}>
-          <Card sx={{ maxWidth: 345 }}>
+          <Card>
             <CardHeader title={item.title} subheader="" />
             {item.imgLink ? (
               <CardMedia
@@ -47,8 +26,6 @@ export default function Monday() {
                 alt={item.title}
               />
             ) : (
-              // <iframe width="320" height="240" src={item.videoLink} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-              // <iframe width="320" height="240" src={item.videoLink}></iframe>
               <ReactPlayer url={item.videoLink} width="320" height="240" controls={true} />
             )}
             <CardContent>
